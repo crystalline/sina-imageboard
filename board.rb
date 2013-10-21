82f4e9f47e3724216fd2e51f1b4bcb429255a22e
Crystalline Emerald <crystalline.emerald@gmail.com>
2013-10-21 22:55:12 +0400

    threads and posts work

--- board.rb
+++ board.rb
@@ -46,7 +46,67 @@ class Board < Sinatra::Base
 #	end
 	
 	get '/board/thread/:tid' do
-		"GET tid #{params[:tid]}"
+		
+		tid = params[:tid]
+		
+		posts = $db['posts'].find(:tid => BSON::ObjectId.from_string(tid)).to_a
+		
+		thread_html = ""
+		
+		for post in posts
+			thread_html += (erb("<tr><td>
+									<div class='post'>
+										<b>
+										   <%= post['name'] %>
+										   <%= post['created_at'].ctime %>
+										</b>
+										<div class='post_txt'><%= post['msg'] %></div>
+									</div>
+									</td></tr>", :locals => {:post => post}))
+		end
+		
+		action = "/board/thread/#{tid}/newpost"
+		
+		code = erb("<html>
+				<head>
+					<title>Rubychan</title>
+					<link href='/ice.css' type='text/css' rel='stylesheet'/>
+					<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+				</head>
+			<body>
+			<div id='header'>
+			Aurelia unlimited
+			</div>
+			<div id='submitform'>
+			<center>
+			<p style='font-size: 30px; font-style: italic'>Create new post</p>
+			<form name='input' action=<%= action %> method='post'>
+				<table id='inptab'>
+				<tr>
+					<td>Name</td>
+					<td><input class='txtin' type='text' name='name' ></td>
+				</tr>
+				<tr>
+					<td>Email</td>
+					<td><input class='txtin' type='text' name='email' ></td>
+				</tr>
+				<tr>
+					<td>Message</td>
+					<td><textarea class='txtin' name='msg' rows='10' ></textarea></td>
+				</tr>
+				<tr>
+					<td>Captcha</td>
+					<td><input class='txtin' type='text' name='captcha'></td>
+				</tr>
+				</table>
+				<input class='button' type='submit' value='Submit'>
+			</form>
+			</center>
+			</div>
+			<table>", :locals => {:action => action}) + thread_html +  "</table></body></html>"
+			
+			erb code
+		
 	end
 	
 	get '/board' do
@@ -63,17 +123,18 @@ class Board < Sinatra::Base
 
 			posts = thread[1]
 			first_post = posts[0]
-
+			tid = thread[0]['_id']
+			
 			thread_html += (erb("<tr><td>
 									<div class='head_post'>
 										<b>
 								   		<%= first_post['name'] %>
 								   		<%= first_post['created_at'].ctime %>
-								   		&nbsp
+								   		<a href=/board/thread/<%= tid %>>Reply</a>
 										</b>
 									<div class='post_txt'><%= first_post['msg'] %></div>
 									</div>
-									</td></tr>", :locals => {:first_post => first_post}))
+									</td></tr>", :locals => {:first_post => first_post, :tid => tid}))
 			if posts.size > 1 then
 				for post in posts[1, posts.size-1]
 					thread_html += (erb("<tr><td>
@@ -97,7 +158,7 @@ class Board < Sinatra::Base
 				</head>
 			<body>
 			<div id='header'>
-			<%= $greeting %>
+			Aurelia unlimited
 			</div>
 			<div id='submitform'>
 			<center>
@@ -129,6 +190,38 @@ class Board < Sinatra::Base
 		erb code
 	end
 	
+	post '/board/thread/:tid/newpost' do
+		t = Time.now
+		
+		#puts params[:tid]
+		
+		if params[:msg] == "" then
+		    puts "[Empty post #{t}]"
+		    redirect '/empty_post'
+		end
+		
+		if params[:name] == "" then
+		    params[:name] = $default_name
+		end
+		
+		puts "[New post #{t}]"
+		
+		tid = params[:tid]
+		
+		#Create new post document
+		$db['posts'].insert(
+		  :name      => params[:name],
+		  :msg       => params[:msg],
+		  :email     => params[:email],
+		  :created_at => t,
+		  :tid => BSON::ObjectId.from_string(tid)
+		)
+		
+		#Return back to 
+		redirect "/board/thread/#{tid}"
+
+	end
+	
 	post '/newthread' do
 		t = Time.now
 		
@@ -141,7 +234,7 @@ class Board < Sinatra::Base
 		    params[:name] = $default_name
 		end
 		
-		puts "[New post #{t}]"
+		puts "[New thread #{t}]"
 		
 		tid = $db['threads'].insert(
 		  :created_at => t
